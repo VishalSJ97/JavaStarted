02938c58176e67901434d5043205a7b74c5282af
Vishal Jadhav <vsjjadhav99@gmail.com>
2022-06-18 15:01:55 -0500

    new code added

--- JavaStarted/src/main/java/com/CH1/Code6.java
+++ JavaStarted/src/main/java/com/CH1/Code6.java
@@ -1,2 +1,17 @@
-package com.CH1;public class Code6 {
+package com.CH1;
+
+/**
+ * Q6.
+ *   Accept values via CLA*  for 1-D array of integer type and display it on screen
+ * */
+
+public class Code6 {
+    public static void main(String[] args) {
+        int size = Integer.parseInt(args[0]);
+        int[] a = new int[size];
+        for(int i=1; i<=size; i++)
+            a[i-1] = Integer.parseInt(args[i]);
+        for(int i=1; i<=size; i++)
+            System.out.print(a[i-1]+" ");
+    }
 }
