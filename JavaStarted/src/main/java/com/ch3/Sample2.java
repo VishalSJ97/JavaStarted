05bca321ca556e073ed4ddab55019c1518664d92
Vishal Jadhav <vsjjadhav99@gmail.com>
2022-06-18 12:41:29 -0500

    addition code added

---
+++ JavaStarted/src/main/java/com/ch3/Sample2.java
@@ -0,0 +1,10 @@
+package com.ch3;
+
+public class Sample2 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        System.out.println("ADDITION IS :: "+(a+b));
+    }
+}
