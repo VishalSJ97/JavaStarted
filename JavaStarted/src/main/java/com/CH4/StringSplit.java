417f77166cbdfd9310a0692b2c9f9a514ae54d4a
Vishal Jadhav <vsjjadhav99@gmail.com>
2022-06-19 21:35:52 -0500

    new chapter added

--- JavaStarted/src/main/java/com/CH4/StringSplit.java
+++ JavaStarted/src/main/java/com/CH4/StringSplit.java
@@ -1,4 +1,19 @@
 package com.CH4;
 
 public class StringSplit {
+    public static void main(String[] args) {
+        String withSpace = "This is the line with Spaces";
+        String withDash = "This-is-the-line-with-dash";
+
+        String[] withSpaces = withSpace.split(" ");
+        for(String str : withSpaces) {
+            System.out.println(str);
+        }
+
+        System.out.println("=================================");
+        String[] withDashes = withDash.split("-");
+        for(String str : withDashes) {
+            System.out.println(str);
+        }
+    }
 }
