20c0ee380e259861f1a5ab0f4dd4b795c12edecd
Vishal Jadhav <vsjjadhav99@gmail.com>
2023-01-20 08:23:04 -0500

    new Lesson2 added

---
+++ Lab-01-Start Core/src/com/app/book/Lesson2.java
@@ -0,0 +1,28 @@
+package com.app.book;
+
+/**
+ * Converting one data type to another
+ * Widening Casting (automatically) - converting a smaller type to a larger type size
+ * byte -> short -> char -> int -> long -> float -> double
+ *
+ * Narrowing Casting (manually) - converting a larger type to a smaller size type
+ * double -> float -> long -> int -> char -> short -> byte
+ * */
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        //Widening
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+
+        System.out.println(myInt);      // Outputs 9
+        System.out.println(myDouble);   // Outputs 9.0
+
+        //narrowing
+        myDouble = 9.78d;
+        myInt = (int) myDouble; // Manual casting: double to int
+
+        System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt);      // Outputs 9
+    }
+}
